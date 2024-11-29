bc9f311b69165b6ab1760dc83abd4c1130eb888e
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-29 13:31:12 +0530

    Initial commit

---
+++ src/main/java/com/example/Quora_Backend_System/QuoraBackendSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Quora_Backend_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoraBackendSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoraBackendSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Quora_Backend_System/QuoraBackendSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Quora_Backend_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoraBackendSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
