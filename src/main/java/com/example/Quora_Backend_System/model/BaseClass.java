9a14cabab4e804579c3ad1d48a99a04b0bef645a
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-29 15:13:31 +0530

    Added the attributes for the Base class

--- src/main/java/com/example/Quora_Backend_System/model/BaseClass.java
+++ src/main/java/com/example/Quora_Backend_System/model/BaseClass.java
@@ -1,4 +1,29 @@
 package com.example.Quora_Backend_System.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false,updatable = false)
+    private Long id;
+
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
 }
