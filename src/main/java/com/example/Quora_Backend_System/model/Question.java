9bd36249ed33f234e08aab6c55521bdb4ea5e5bd
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-30 11:47:30 +0530

    Added the attributes for the Question class

--- src/main/java/com/example/Quora_Backend_System/model/Question.java
+++ src/main/java/com/example/Quora_Backend_System/model/Question.java
@@ -1,4 +1,32 @@
 package com.example.Quora_Backend_System.model;
 
-public class Question {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Question extends BaseClass {
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    @OneToOne
+    private User user;
+
+    @ManyToOne
+    private List<Answer> answers;
+
+    @ManyToOne
+    private List<Comment> comments;
+
 }
