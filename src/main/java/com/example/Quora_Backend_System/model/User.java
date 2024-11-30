7143545aadd3bae266f02a721ee7f021ffedbbd0
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-30 12:01:58 +0530

    Added the attributes for the User class

--- src/main/java/com/example/Quora_Backend_System/model/User.java
+++ src/main/java/com/example/Quora_Backend_System/model/User.java
@@ -1,5 +1,33 @@
 package com.example.Quora_Backend_System.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseClass{
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Question> questions;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Answer> answers;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Comment> comments;
 
 }
