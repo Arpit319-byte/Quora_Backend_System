a163d6f04cb0fbe1cfce6538e55277dc9558ff13
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-30 14:23:41 +0530

    Added the CRUD Apis for the User Controller

--- src/main/java/com/example/Quora_Backend_System/controller/UserController.java
+++ src/main/java/com/example/Quora_Backend_System/controller/UserController.java
@@ -1,4 +1,44 @@
 package com.example.Quora_Backend_System.controller;
 
+import com.example.Quora_Backend_System.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable Long userId) {
+        return userService.getUserById(userId);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{userId}")
+    public User updateUser(@PathVariable Long userId, @RequestBody User user) {
+        return userService.updateUser(userId, user);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+    }
 }
