fd81b3d098a8ec4df0ec51145e82a616e394f1fd
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-11-30 23:46:52 +0530

    Added the CRUD Api's for the Answer class

--- src/main/java/com/example/Quora_Backend_System/controller/AnswerController.java
+++ src/main/java/com/example/Quora_Backend_System/controller/AnswerController.java
@@ -1,4 +1,46 @@
 package com.example.Quora_Backend_System.controller;
 
+import com.example.Quora_Backend_System.model.Answer;
+import com.example.Quora_Backend_System.service.AnswerService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/answers")
 public class AnswerController {
+
+    private final AnswerService answerService;
+
+    public AnswerController(AnswerService answerService) {
+        this.answerService = answerService;
+    }
+
+    @GetMapping
+    public List<Answer> getAllAnswers() {
+        return answerService.getAllAnswers();
+    }
+
+    @GetMapping("/{answerId}")
+    public Answer getAnswerById(@PathVariable Long answerId) {
+        return answerService.getAnswerById(answerId);
+    }
+
+    @PostMapping
+    public Answer createAnswer(@RequestBody Answer answer) {
+        return answerService.createAnswer(answer);
+    }
+
+    @PutMapping("/{answerId}")
+    public Answer updateAnswer(@PathVariable Long answerId, @RequestBody Answer answer) {
+        return answerService.updateAnswer(answerId, answer);
+    }
+
+    @DeleteMapping("/{answerId}")
+    public void deleteAnswer(@PathVariable Long answerId) {
+        answerService.deleteAnswer(answerId);
+    }
+
 }
--- src/main/java/com/example/Quora_Backend_System/service/AnswerService.java
+++ src/main/java/com/example/Quora_Backend_System/service/AnswerService.java
@@ -1,4 +1,21 @@
 package com.example.Quora_Backend_System.service;
 
+import com.example.Quora_Backend_System.model.Answer;
+import java.util.List;
+
 public class AnswerService {
+    public List<Answer> getAllAnswers() {
+    }
+
+    public Answer getAnswerById(Long answerId) {
+    }
+
+    public Answer createAnswer(Answer answer) {
+    }
+
+    public Answer updateAnswer(Long answerId, Answer answer) {
+    }
+
+    public void deleteAnswer(Long answerId) {
+    }
 }
