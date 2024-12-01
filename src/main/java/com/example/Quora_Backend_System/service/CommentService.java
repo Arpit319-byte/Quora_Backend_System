23c80d6dbeb270d1026b85371706a5cc65b5e700
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-01 11:58:20 +0530

    Added the CRUD functions in Comment Service class

--- src/main/java/com/example/Quora_Backend_System/service/CommentService.java
+++ src/main/java/com/example/Quora_Backend_System/service/CommentService.java
@@ -1,7 +1,24 @@
 package com.example.Quora_Backend_System.service;
 
+import com.example.Quora_Backend_System.model.Comment;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CommentService {
+    public List<Comment> getAllComments() {
+    }
+
+    public Comment getCommentById(Long commentId) {
+    }
+
+    public Comment createComment(Comment comment) {
+    }
+
+    public Comment updateComment(Long commentId, Comment comment) {
+    }
+
+    public boolean deleteComment(Long commentId) {
+    }
 }
