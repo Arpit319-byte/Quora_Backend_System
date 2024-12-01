234838ffa943942091a6e1e35716c2c5c122ae59
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-01 11:18:47 +0530

    Added the Comment Repository class

--- src/main/java/com/example/Quora_Backend_System/repository/CommentRepository.java
+++ src/main/java/com/example/Quora_Backend_System/repository/CommentRepository.java
@@ -1,4 +1,9 @@
 package com.example.Quora_Backend_System.repository;
 
-public class CommentRepository {
+import com.example.Quora_Backend_System.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
