e7f49801fc9a048a2f311a9ebbb9e7867f4e1179
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-14 11:12:01 +0530

    Added the attributes for the comment class

--- src/main/java/com/example/Quora_Backend_System/model/Comment.java
+++ src/main/java/com/example/Quora_Backend_System/model/Comment.java
@@ -1,4 +1,35 @@
 package com.example.Quora_Backend_System.model;
 
-public class Comment {
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Comment extends BaseClass {
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ParentType parentType;
+
+    public enum ParentType {
+        QUESTION,
+        ANSWER
+    }
+
 }
