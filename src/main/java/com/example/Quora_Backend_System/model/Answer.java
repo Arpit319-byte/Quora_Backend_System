f2fa5dcdbaae86d610044ae86b3f0d44b839e63a
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-14 15:03:32 +0530

    Added the cascade functionality for the related fields

--- src/main/java/com/example/Quora_Backend_System/model/Answer.java
+++ src/main/java/com/example/Quora_Backend_System/model/Answer.java
@@ -25,12 +25,12 @@ public class Answer extends BaseClass {
     @JoinColumn(name = "question_id")
     private Question question;
 
-    @OneToMany(mappedBy = "answer")
+    @OneToMany(mappedBy = "answer",cascade = CascadeType.ALL)
     private List<Comment> comment;
 
-    @OneToMany(mappedBy = "vote")
+    @OneToMany(mappedBy = "VoteType",cascade = CascadeType.ALL)
     private List<Vote> votes;
 
-    @OneToMany(mappedBy = "report")
+    @OneToMany(mappedBy = "report",cascade = CascadeType.ALL)
     private List<Report> reports;
 }
