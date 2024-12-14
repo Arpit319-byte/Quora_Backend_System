68fe0e76789fac4a72ff97b1a960344be54d4da5
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-14 11:00:11 +0530

    Added the CRUD methods in the Question Service class

--- src/main/java/com/example/Quora_Backend_System/service/CommentService.java
+++ src/main/java/com/example/Quora_Backend_System/service/CommentService.java
@@ -1,25 +1,79 @@
 package com.example.Quora_Backend_System.service;
 
 import com.example.Quora_Backend_System.model.Comment;
+import com.example.Quora_Backend_System.repository.CommentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CommentService {
+
+    private final static Logger logger = LoggerFactory.getLogger(CommentService.class);
+
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
     public List<Comment> getAllComments() {
+        logger.info("Getting all comments from the database");
+        return commentRepository.findAll();
     }
 
     public Comment getCommentById(Long commentId) {
+        logger.info("Getting comment by id - {} from the database", commentId);
+        try {
+            Comment comment = commentRepository.findById(commentId).orElse(null);
+            if (comment == null) {
+                logger.error("Comment not found for id - {}", commentId);
+                return null;
+            }
+            return comment;
+        }catch(Exception e) {
+            logger.error("Error occurred while getting comment by id - {}", commentId, e);
+            return null;
+        }
     }
 
     public Comment createComment(Comment comment) {
+
+        logger.info("Creating comment in the database");
+        try {
+            return commentRepository.save(comment);
+        }catch(Exception e) {
+            logger.error("Error occurred while creating comment in the database", e);
+            return null;
+        }
+
     }
 
     public Comment updateComment(Long commentId, Comment comment) {
+
+        logger.info("Updating comment in the database");
+        try {
+            Comment existingComment = commentRepository.findById(commentId).orElse(null);
+            if (existingComment == null) {
+                logger.error("Comment not found for id - {}", commentId);
+                return null;
+            }
+            existingComment.setContent(comment.getContent());
+            existingComment.setQuestion(comment.getQuestion());
+            existingComment.setUser(comment.getUser());
+            existingComment.setAnswer(comment.getAnswer());
+            existingComment.setComment(comment.getComment());
+            return commentRepository.save(comment);
+        }catch (Exception e) {
+            logger.error("Error occurred while updating comment in the database", e);
+            return null;
+        }
     }
 
     public boolean deleteComment(Long commentId) {
+
         return false;
     }
 }
--- src/main/java/com/example/Quora_Backend_System/service/QuestionService.java
+++ src/main/java/com/example/Quora_Backend_System/service/QuestionService.java
@@ -1,22 +1,89 @@
 package com.example.Quora_Backend_System.service;
 
 import com.example.Quora_Backend_System.model.Question;
+import com.example.Quora_Backend_System.repository.QuestionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class QuestionService {
+
+    private final static Logger logger = LoggerFactory.getLogger(QuestionService.class);
+
+    private final QuestionRepository questionRepository;
+
+    @Autowired
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
+
+
     public List<Question> getAllQuestions() {
+        logger.info("Getting all questions from the database");
+        return questionRepository.findAll();
     }
 
     public Question getQuestionById(Long questionId) {
+        logger.info("Getting question by id - {} from the database", questionId);
+
+        try {
+            Question question = questionRepository.findById(questionId).orElse(null);
+            if (question == null) {
+                logger.error("Question not found for id - {}", questionId);
+                return null;
+            }
+            return question;
+        } catch (Exception e) {
+            logger.error("Error occurred while getting question by id - {}", questionId, e);
+            return null;
+        }
     }
 
     public Question createQuestion(Question question) {
+        logger.info("Creating question in the database");
+
+        try {
+            return questionRepository.save(question);
+        } catch (Exception e) {
+            logger.error("Error occurred while creating question in the database", e);
+            return null;
+        }
     }
 
     public Question updateQuestion(Long questionId, Question question) {
+        logger.info("Updating question in the database");
+
+        try {
+            Question existingQuestion = questionRepository.findById(questionId).orElse(null);
+            if (existingQuestion == null) {
+                logger.error("Question not found for id - {}", questionId);
+                return null;
+            }
+            existingQuestion.setContent(question.getContent());
+            return questionRepository.save(existingQuestion);
+        } catch (Exception e) {
+            logger.error("Error occurred while updating question in the database", e);
+            return null;
+        }
     }
 
     public Boolean deleteQuestion(Long questionId) {
+
+        logger.info("Deleting question from the database with id- {}", questionId);
+
+        try {
+            Question question = questionRepository.findById(questionId).orElse(null);
+            if (question == null) {
+                logger.error("Question not found for id - {}", questionId);
+                return false;
+            }
+            questionRepository.deleteById(questionId);
+            return true;
+        } catch (Exception e) {
+            logger.error("Error occurred while deleting question from the database with id- {}", questionId, e);
+            return false;
+        }
     }
 }
