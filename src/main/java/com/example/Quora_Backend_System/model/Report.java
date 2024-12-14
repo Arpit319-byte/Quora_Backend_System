a9b38ecfba9a02f94b5f9707b6f5474e8c0dab09
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-14 11:48:02 +0530

    Added the attirbutes for the Report class

--- src/main/java/com/example/Quora_Backend_System/model/Report.java
+++ src/main/java/com/example/Quora_Backend_System/model/Report.java
@@ -1,4 +1,39 @@
 package com.example.Quora_Backend_System.model;
 
-public class Report {
+import jakarta.persistence.*;
+import jdk.jshell.Snippet;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Report extends BaseClass{
+
+    @Column(nullable = false)
+    private String reason;
+
+    @Column(nullable = false)
+    private String details;
+
+    @ManyToOne
+    private User reportedBy;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TargetEntity targetEntity;
+
+    @Column(nullable = false)
+    private Long targetId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+
 }
