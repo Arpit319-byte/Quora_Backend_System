66b06000065142272060f99a5c685d2071ec8547
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 17:06:28 +0530

    Added the attributes for the QuestionDTO class

--- src/main/java/com/example/Quora_Backend_System/DTOS/QuestionDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTOS/QuestionDTO.java
@@ -1,4 +1,10 @@
 package com.example.Quora_Backend_System.DTOS;
 
+import lombok.Data;
+
+@Data
 public class QuestionDTO {
+    private Long id;
+    private String content;
+    private Long userId;
 }
