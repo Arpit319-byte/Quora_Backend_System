b6d30f68005e2a6a5b5df6e219e3f8559d63d7c6
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 17:50:36 +0530

    Added the attributes for the VoteDTO class

--- src/main/java/com/example/Quora_Backend_System/DTOS/VoteDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTOS/VoteDTO.java
@@ -1,4 +1,12 @@
 package com.example.Quora_Backend_System.DTOS;
 
+import lombok.Data;
+
+@Data
 public class VoteDTO {
+    private Long id;
+    private Long userId;
+    private Long parentId;
+    private String parentType;
+    private Integer voteType;
 }
