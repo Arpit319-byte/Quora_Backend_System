25d3d9f5dba5f7bbca24b213fec583ff0ee52a67
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 17:48:24 +0530

    Added attributes for the ReportDTO class

--- src/main/java/com/example/Quora_Backend_System/DTOS/ReportDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTOS/ReportDTO.java
@@ -1,4 +1,14 @@
 package com.example.Quora_Backend_System.DTOS;
 
+import lombok.Data;
+
+@Data
 public class ReportDTO {
+    private Long id;
+    private Long userId;
+    private Long answerId;
+    private Long questionId;
+    private String content;
+    private String type;
+    private String status;
 }
