2eae6e2d159b1532aa6c06da03518bfbfbfd170b
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 17:03:45 +0530

    Added the attributes for the AnswerDTO class

--- src/main/java/com/example/Quora_Backend_System/DTOS/AnswerDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTOS/AnswerDTO.java
@@ -1,4 +1,11 @@
 package com.example.Quora_Backend_System.DTOS;
 
+import lombok.Data;
+
+@Data
 public class AnswerDTO {
+    private Long id;
+    private String content;
+    private Long userId;
+    private Long questionId;
 }
