c932e83a3622299b9e423e48f7cf92a406632dcf
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 18:15:20 +0530

    Added the validations for the AnswerDTO class

--- src/main/java/com/example/Quora_Backend_System/DTOS/AnswerDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTOS/AnswerDTO.java
@@ -1,11 +1,22 @@
 package com.example.Quora_Backend_System.DTOS;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AnswerDTO {
+
     private Long id;
+
+    @NotBlank(message = "Answer Content cannot be empty")
+    @Size(min=2, max=3000, message = "Answer Content must be between 2 and 3000 characters")
     private String content;
+
+    @NotNull(message = "User Id cannot be empty")
     private Long userId;
+
+    @NotNull(message = "Question Id cannot be empty")
     private Long questionId;
 }
