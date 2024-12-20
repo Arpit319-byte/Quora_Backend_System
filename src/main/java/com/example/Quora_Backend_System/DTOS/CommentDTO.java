81394e3a128ef17835b6bc8ff567ea07dd015d89
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 17:12:40 +0530

    Added the attributes for the CommentDTO class

--- src/main/java/com/example/Quora_Backend_System/DTOS/CommentDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTOS/CommentDTO.java
@@ -1,4 +1,12 @@
 package com.example.Quora_Backend_System.DTOS;
 
+import lombok.Data;
+
+@Data
 public class CommentDTO {
+    private Long id;
+    private String content;
+    private Long userId;
+    private Long answerId;
+    private Long parentCommentId;
 }
