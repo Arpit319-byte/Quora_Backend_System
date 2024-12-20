2c2ed17f1d607443d8c6c13cea4eedb13de31d62
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 18:43:25 +0530

    Removed the Report functionality

--- src/main/java/com/example/Quora_Backend_System/repository/ReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Quora_Backend_System.repository;
-
-import com.example.Quora_Backend_System.model.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReportRepository extends JpaRepository<Report,Long> {
-}
