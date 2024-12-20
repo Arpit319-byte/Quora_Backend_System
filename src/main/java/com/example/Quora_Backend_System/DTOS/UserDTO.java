10cbb2f3431460a181a7b5402a59088eb592f1e2
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 18:00:40 +0530

    Added the validation for the UserDTO class

--- src/main/java/com/example/Quora_Backend_System/DTOS/UserDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTOS/UserDTO.java
@@ -1,11 +1,19 @@
 package com.example.Quora_Backend_System.DTOS;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserDTO {
 
     private Long id;
+
+    @NotBlank(message = "User Name cannot be empty")
+    @Size(min=2, max=50, message = "User Name must be between 2 and 50 characters")
     private String userName;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min=6, max=50, message = "Password must be between 6 and 50 characters")
     private String password;
 }
