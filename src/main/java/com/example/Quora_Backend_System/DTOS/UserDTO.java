084a932e4465c3c5d14d0413a964d522e47e394b
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 16:58:35 +0530

    Added the attributes for the UserDTO

--- src/main/java/com/example/Quora_Backend_System/DTOS/UserDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTOS/UserDTO.java
@@ -1,4 +1,11 @@
 package com.example.Quora_Backend_System.DTOS;
 
+import lombok.Data;
+
+@Data
 public class UserDTO {
+
+    private Long id;
+    private String userName;
+    private String password;
 }
