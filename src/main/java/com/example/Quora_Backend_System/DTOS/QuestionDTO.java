03009a0544163c960c8bd6445ce982ccfa84a320
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 18:08:03 +0530

    Added the validation for the QuestionDTO class

--- src/main/java/com/example/Quora_Backend_System/DTOS/QuestionDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTOS/QuestionDTO.java
@@ -1,10 +1,19 @@
 package com.example.Quora_Backend_System.DTOS;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class QuestionDTO {
+
     private Long id;
+
+    @NotBlank(message = "Question Content cannot be empty")
+    @Size(min=2, max=3000, message = "Question Content must be between 2 and 3000 characters")
     private String content;
+
+    @NotNull(message = "User Id cannot be empty")
     private Long userId;
 }
