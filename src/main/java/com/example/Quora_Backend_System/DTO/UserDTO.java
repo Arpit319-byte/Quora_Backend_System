698796dd5d02b61a8bbe7091e47d0d2f4ca53ff6
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 19:12:35 +0530

    Renamed the package to DTO

--- src/main/java/com/example/Quora_Backend_System/DTO/UserDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTO/UserDTO.java
@@ -1,4 +1,4 @@
-package com.example.Quora_Backend_System.DTOS;
+package com.example.Quora_Backend_System.DTO;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
