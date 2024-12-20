372d911b215159dbd4355e97820b51e2f6ac1614
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 18:37:57 +0530

    Added the validation for the CommentDTO class

--- src/main/java/com/example/Quora_Backend_System/DTOS/CommentDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTOS/CommentDTO.java
@@ -1,12 +1,23 @@
 package com.example.Quora_Backend_System.DTOS;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDTO {
+
     private Long id;
+
+    @NotBlank(message = "Comment Content cannot be empty")
+    @Size(min=2, max=3000, message = "Comment Content must be between 2 and 3000 characters")
     private String content;
+
+    @NotNull
     private Long userId;
+
+    @NotNull
     private Long answerId;
     private Long parentCommentId;
 }
