8606d019405487958147c5da9e6600d982ce7df9
Arpit319-byte <78552243+Arpit319-byte@users.noreply.github.com>
2024-12-20 18:55:52 +0530

    Added the validation for the VoteDTO class

--- src/main/java/com/example/Quora_Backend_System/DTOS/VoteDTO.java
+++ src/main/java/com/example/Quora_Backend_System/DTOS/VoteDTO.java
@@ -1,12 +1,24 @@
 package com.example.Quora_Backend_System.DTOS;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class VoteDTO {
+
     private Long id;
+
+    @NotBlank(message = "Vote type is required")
+    @Size(max = 10, message = "Vote type can't be more than 10 characters")
+    private String voteType;
+
+    @NotNull(message = "User id is required")
     private Long userId;
-    private Long parentId;
-    private String parentType;
-    private Integer voteType;
+
+
+    private Long answerId;
+    private Long questionId;
+    private Long commentId;
 }
